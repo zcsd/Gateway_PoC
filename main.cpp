f1de283315bd2cfdd55463d883e4ec4404ca887a
zichun <zichun92@gmail.com>
2018-11-07 14:22:47 +0800

    change project name

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "hmiauthorization.h"
+#include "gateway.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    HmiAuthorization w;
+    Gateway w;
     w.show();
 
     return a.exec();
